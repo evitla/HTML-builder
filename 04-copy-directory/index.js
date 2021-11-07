5f8f90b12e6a38a850f3c502d268f0bb175bf319
evitla <s.dinmuhamed@gmail.com>
2021-11-07 11:21:27 +0600

    feat: solve 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyFile = (src, dest) => {
+  fs.copyFile(src, dest, (err) => {
+    if (err) return console.error(err.message);
+  });
+};
+
+const copyDir = (src, dest) => {
+  fs.mkdir(dest, { recursive: true }, (err) => {
+    if (err) return console.error(err.message);
+  });
+
+  fs.readdir(src, (err, files) => {
+    if (err) return console.error(err.message);
+
+    for (const file of files) copyFile(path.join(src, file), path.join(dest, file));
+  });
+};
+
+copyDir(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));
