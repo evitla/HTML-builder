149ce4615fe22533089a739bc32ed850943c2c4d
evitla <s.dinmuhamed@gmail.com>
2021-11-07 11:56:11 +0600

    fix: use async functions from fs/promises module

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,21 +1,21 @@
-const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
-const folderPath = path.join(__dirname, 'secret-folder');
+const printFilesInfo = async (src) => {
+  try {
+    const files = await fsPromises.readdir(src);
 
-const printFileInfo = (file) => {
-  fs.stat(path.join(folderPath, file), (err, fileStats) => {
-    if (err) return console.error(err.message);
+    for (const file of files) {
+      const fileStats = await fsPromises.stat(path.join(src, file));
 
-    if (fileStats.isFile()) {
-      const ext = path.extname(file);
-      console.log(`${file.replace(ext, '')} - ${ext.slice(1)} - ${fileStats.size / 1000}kb`);
+      if (fileStats.isFile()) {
+        const ext = path.extname(file);
+        console.log(`${file.replace(ext, '')} - ${ext.slice(1)} - ${fileStats.size / 1000}kb`);
+      }
     }
-  });
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
-fs.readdir(folderPath, (err, files) => {
-  if (err) return console.error(err.message);
-
-  for (const file of files) printFileInfo(file);
-});
+printFilesInfo(path.join(__dirname, 'secret-folder'));
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,22 +1,17 @@
-const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 
-const copyFile = (src, dest) => {
-  fs.copyFile(src, dest, (err) => {
-    if (err) return console.error(err.message);
-  });
-};
+const copyDir = async (src, dest) => {
+  try {
+    await fsPromises.mkdir(dest, { recursive: true });
 
-const copyDir = (src, dest) => {
-  fs.mkdir(dest, { recursive: true }, (err) => {
-    if (err) return console.error(err.message);
-  });
-
-  fs.readdir(src, (err, files) => {
-    if (err) return console.error(err.message);
-
-    for (const file of files) copyFile(path.join(src, file), path.join(dest, file));
-  });
+    const files = await fsPromises.readdir(src);
+    for (const file of files) {
+      await fsPromises.copyFile(path.join(src, file), path.join(dest, file));
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 copyDir(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'));
